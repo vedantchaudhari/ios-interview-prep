1748aedd39a658e3dd0a585cd2edaf7d2153c139
Vedant Chaudhari <vedantchaudhari@outlook.com>
2022-02-17 20:53:08 -0500

    Added generic stack data structure

---
+++ Stack.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+struct Stack<Element> {
+    private var items: [Element] = []
+    
+    func peek() -> Element {
+        guard let topItem = items.first else {
+            fatalError("This stack contains no items!")
+        }
+        
+        return topItem
+    }
+    
+    // Insertion and Deletion have O(1) time complexity
+    mutating func insert(_ item: Element) {
+        items.append(item)
+    }
+    
+    mutating func remove() -> Element {
+        items.removeLast()
+    }
+}
+
+var carStack = Stack<String>()
+carStack.insert("Audi")
+carStack.insert("BMW")
+carStack.insert("Mercedes")
+carStack.insert("Volkswagen")
+
+print("Starting Data Structure:", carStack)
+print("Peek top:", carStack.peek())
+print("Pop/Remove (FILO) item:", carStack.remove())
+print("New Data Structure", carStack)
